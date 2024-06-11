a577a1d41ff03b6120da9bbcca586abc7ff2ab34
AnujaKalyani <kalyanianuja24@gmail.com>
2024-06-11 15:09:51 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,14 @@
+package day2_b;
+
+public class A {
+
+	public static void main(String[] args) 
+	{
+		//System.out.println("hello");
+		//A1 fd =new A1();  // object creation
+		
+		A1.add();
+		A1.sub(24,30);
+	}
+
+}
---
+++ A1.java
@@ -0,0 +1,26 @@
+package day2_b;
+
+public class A1 
+{
+public static void add() {
+	int b=10;
+	int a=20;
+	int total=a+b;
+	
+	System.out.println("addition :"+total);
+	
+}
+
+//OR 
+
+public static void sub(int a , int b) {
+	//int b=10;
+	//int a=20;
+	int total=a-b;
+	
+	
+	
+	System.out.println("substraction :"+total);
+}
+	
+}
